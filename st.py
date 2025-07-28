70a4da49094cf61b9fd55665d5060456885c249b
Akshat <akshatjangid9@gmail.com>
2025-07-28 21:02:16 +0530

    update file

--- st.py
+++ st.py
@@ -86,7 +86,7 @@ elif page == "📄 Resume":
                         mime="application/pdf"
                         ) 
     
-    with open("AKSHAT JANGID NEW RESUME  .pdf", "rb") as file:
+    with open("resume.png", "rb") as file:
         base64_pdf = base64.b64encode(file.read()).decode('utf-8')
         pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="850" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
